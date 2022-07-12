44ca9f55edfb08e3becf93d0baac09e251913ccf
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-07-12 18:08:07 +0200

    1.0-beta.3 (4)

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,28 +1,10 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
 	name: "ready",
 	once: true,
 	async run(client) {
 		console.log(`Bot connecté en tant que ${client.user.tag} !`);
-
-		let i = 0;
-        const status = [
-			["WATCHING", `${client.playerDb.db.array().map(e => e.started)} joueurs`],
-			["WATCHING", `${client.guilds.cache.size} serveurs`],
-			["COMPETING", `version ${client.version}`],
-        ];
-		setInterval(() => {
-			client.user.setPresence({
-				status: "online",
-				afk: false,
-				activities: [
-					{
-						name: status[i][1],
-						type: status[i][0],
-					},
-				],
-			});
-			i++;
-			if (i === status.length) i = 0;
-		}, 15000);
+		client.user.setPresence({ activities: [{ name: `version ${client.version}`, type: ActivityType.Watching }], status: "online" });
 	},
 };
