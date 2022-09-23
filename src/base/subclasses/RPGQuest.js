d96ac506db46fed10c65a3e6b2d31f1ef01d6908
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-09-23 20:33:34 +0200

    Quest system: initiated the main classes

--- src/base/subclasses/RPGQuest.js
+++ src/base/subclasses/RPGQuest.js
@@ -1,9 +1,9 @@
 const RPGAssetBase = require("./RPGAssetBase");
 
-class RPGQuestObjective extends RPGAssetBase {
+class RPGQuest extends RPGAssetBase {
     constructor(lang, id) {
         super(lang, id);
     }
 }
 
-module.exports = RPGQuestObjective;
+module.exports = RPGQuest;
---
+++ src/base/subclasses/rpg_quest_objectives/TrainStatistic.js
@@ -0,0 +1,9 @@
+const RPGQuestObjective = require("../RPGQuestObjective");
+
+class TrainStatistic extends RPGQuestObjective {
+    constructor(lang, id) {
+        super(lang, id);
+    }
+}
+
+module.exports = TrainStatistic;
