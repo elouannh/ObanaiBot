185be393d44916cf5bb00ffad0480ebb615d2ccd
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-10-14 19:00:21 +0200

    Branch switch test

--- src/base/database/tables/QuestDb.js
+++ src/base/database/tables/QuestDb.js
@@ -235,7 +235,7 @@ class QuestDb extends SQLiteTable {
                     let tutorialStep = true;
                     let tutorialAmount = true;
                     if (o.additionalData.step in userData.additional.rpg.tutorialProgress) {
-
+                        console.log("test");
                     }
                     completedInDepth = tutorialStep && tutorialAmount;
                     break;
