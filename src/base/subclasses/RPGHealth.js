b795d7f7450cb528e5430439dac77a9445944025
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-11-08 17:38:50 +0100

    :sparkles: Created RPGHealth.js class

---
+++ src/base/subclasses/RPGHealth.js
@@ -0,0 +1,13 @@
+class RPGHealth {
+    constructor(amount, lastHeal) {
+        this.amount = amount;
+        this.lastRegen = lastHeal;
+
+        this.fullRegen = lastHeal;
+        if (this.amount < 100) {
+            this.fullRegen += Date.now() + (100 - this.amount) * 60 * 5 * 1000;
+        }
+    }
+}
+
+module.exports = RPGHealth;
