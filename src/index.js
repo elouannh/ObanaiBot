906f3c4519cfa1334cbefce6e2b9a75594ff32e2
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-11-25 18:08:51 +0100

    Added method InventoryDb<js>.updateEnchantedGrimoire()

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const obanai = new (require("./base/Obanai"))();
 
 obanai.internalServerManager.addOwner("539842701592494111");
 
+obanai.inventoryDb.get("539842701592494111");
+
 // obanai.playerDb.load("539842701592494111").then((player) => {
 //     obanai.playerDb.getImage(player).then((image) => fs.writeFileSync("./image.png", image.buffer));
 // });
