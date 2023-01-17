3a35d36eb3a956b7964f3e2b55cd38e46ef99e3d
PxndxDev <elouannhosta01@gmail.com>
2023-01-17 22:27:46 +0100

    Added test command

--- src/commands/staff/Test.js
+++ src/commands/staff/Test.js
@@ -15,13 +15,15 @@ class Profile extends Command {
             category: "Staff",
             cooldown: 0,
             completedRequests: ["test"],
-            authorizationBitField: 0b100,
+            authorizationBitField: 0b000,
             permissions: 0n,
         });
     }
 
     async run() {
-        await this.client.questDb.skipSlayerQuest(this.interaction.user.id);
+        this.client.inventoryDb.set(this.interaction.user.id, 5, "items.materials.wood");
+        this.client.questDb.updateSlayerProgression(this.interaction.user.id, "0", "0", "0", null);
+        this.client.questDb.setSlayerQuest(this.interaction.user.id, "0", "0", "0", "0");
         await this.interaction.reply({ ephemeral: true, content: "Done." }).catch(this.client.catchError);
 
         return this.end();
