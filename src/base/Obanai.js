5771ed6aa2888012717f8fdb613aea47f895de2b
PxndxDev <elouannhosta01@gmail.com>
2023-01-26 22:07:11 +0100

    Fixed inventory

--- src/base/Obanai.js
+++ src/base/Obanai.js
@@ -142,7 +142,7 @@ class Obanai extends Client {
         if (data.hour.length < 2) data.hour = "0" + data.hour;
         if (data.min.length < 2) data.min = "0" + data.min;
         if (data.sec.length < 2) data.sec = "0" + data.sec;
-        console.log(chalk.redBright(`[${data.month}/${data.day}] [${data.hour}:${data.hour}:${data.sec}]  |  Error: ${error.stack}`));
+        console.log(chalk.redBright(`[${data.month}/${data.day}] [${data.hour}:${data.hour}:${data.sec}]  |  Error: ${error.message}`));
     }
 
     async throwError(error, origin) {
