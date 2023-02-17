2276c16f2dcd79d2bb9a821c8d55a3fd24c219c1
PxndxDev <elouannhosta01@gmail.com>
2023-02-17 21:19:02 +0100

    :zap: Added dialogues

---
+++ src/assets/dialogues/d002.js
@@ -0,0 +1,9 @@
+module.exports = {
+    id: "d002",
+    // eslint-disable-next-line no-unused-vars
+    read: async (client, id) => {
+        return [
+            "r001", "r002", "r003",
+        ];
+    },
+};
---
+++ src/assets/dialogues/d003.js
@@ -0,0 +1,9 @@
+module.exports = {
+    id: "d003",
+    // eslint-disable-next-line no-unused-vars
+    read: async (client, id) => {
+        return [
+            "r001", "r002",
+        ];
+    },
+};
